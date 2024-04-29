6d52adf50a9bd745243a388dc14ead231e6d57d4
Max von Berg <max.vonberg.fn@googlemail.com>
2024-04-29 18:38:27 +0200

    new SQLite Wrapper

--- Dockerfile
+++ Dockerfile
@@ -2,25 +2,35 @@
 FROM ubuntu:latest
 
 RUN apt-get update && apt-get install -y \
+    wget \
     g++ \
     git \
     cmake \
     libboost-dev \
     libboost-regex-dev \
-    libasio-dev
+    libasio-dev \
+    libsqlite3-dev
 
 WORKDIR /app
 
 RUN git clone https://github.com/CrowCpp/Crow.git
+RUN git clone https://github.com/SRombauts/SQLiteCpp.git
 
 WORKDIR /app/Crow
 RUN mkdir build && cd build && cmake .. -DCROW_BUILD_EXAMPLES=OFF -DCROW_BUILD_TESTS=OFF && make install
 
+WORKDIR /app/SQLiteCpp
+RUN mkdir build && cd build && cmake .. -DBUILD_SHARED_LIBS=ON && make install
+#RUN mkdir build && cd build && cmake .. && make install
+# COPY libSQLiteCpp.so.0 /usr/local/lib/
+
 WORKDIR /app
 
-COPY . .
+COPY . /app
 
-RUN g++ -o main main.cpp -lpthread
+ENV LD_LIBRARY_PATH=/usr/local/lib
+
+RUN g++ -o main main.cpp -std=c++20 -lpthread -lSQLiteCpp
 
 EXPOSE 9999
 
--- main.cpp
+++ main.cpp
@@ -1,14 +1,61 @@
 #include "crow.h"
+#include <string>
+#include <cstring>
+#include <vector> 
+#include <iostream> 
+#include <SQLiteCpp/SQLiteCpp.h>
 
 
-const int port = 9999;
+const int PORT = 9999;
+const std::string TODO_TABLE_NAME = "Todo";
+const std::string DB_DIR = "/app/todosDB.db";
+
+std::vector<std::string> todos;
+
+void create_table(SQLite::Database &db){
+    db.exec("CREATE TABLE IF NOT EXISTS todos (name TEXT)");
+    CROW_LOG_INFO << "Table created...";
+}
+
+void insert_into_table(SQLite::Database &db, std::string name){
+    CROW_LOG_INFO << "Adding " + name + " to DB";
+    db.exec("INSERT into todos (name) VALUES ('" + name + "')");
+    CROW_LOG_INFO << name  << " added to DB";
+}
 
 int main() {
-    crow::SimpleApp app;
 
-    CROW_ROUTE(app, "/")([](){
-        return "Hello world!";
+    crow::SimpleApp app;
+    app.loglevel(crow::LogLevel::Info);
+
+    SQLite::Database db(DB_DIR, SQLite::OPEN_READWRITE | SQLite::OPEN_CREATE);
+    create_table(db);
+
+    CROW_ROUTE(app, "/todo/<string>").methods(crow::HTTPMethod::GET, crow::HTTPMethod::POST)([&db](const crow::request& req,const std::string& name){
+     if (req.method == crow::HTTPMethod::GET) {
+
+            return crow::response(200, "Get Response: " + name);
+        } else if (req.method == crow::HTTPMethod::POST) {
+            insert_into_table(db, name);
+
+            return crow::response(200, "Post Response: " + name);
+        }
+        return crow::response(405); 
     });
 
-    app.port(port).multithreaded().run();
+    CROW_ROUTE(app, "/todos/").methods(crow::HTTPMethod::GET)([&db](){
+        crow::json::wvalue result;
+
+        SQLite::Statement query(db, "SELECT name FROM todos");
+
+        int count = 0;
+        while(query.executeStep()) {
+            result[count] = query.getColumn("name").getText();
+            count++;
+        }
+        return crow::response{result};
+    });
+
+    app.port(PORT).multithreaded().run();
 }
+
